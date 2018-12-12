a73e0aa139b88ebdaf8b2f22039c489dc96b48db
SlickJohnson <liwa.johnson@gmail.com>
2018-12-12 07:53:46 -0800

    Add database

---
+++ data/reddit-db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+assert = require('assert');
+
+const url = 'mongodb://localhost/marked-db';
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  url,
+  { useNewUrlParser: true },
+  (err, db) => {
+    assert.equal(null, err);
+    console.log('Connected successfully to db');
+  },
+);
+mongoose.connection.on(
+  'error',
+  console.error.bind(console, 'MongoDB connection Error:'),
+);
+mongoose.set('debug', true);
+
+module.exports = mongoose.connection;
