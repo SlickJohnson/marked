5afba7aa07d6814cc1b61e77bd237eeb47776d28
SlickJohnson <liwa.johnson@gmail.com>
2018-12-11 18:32:56 -0800

    Add turndown module

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const express = require("express");
+const express = require('express');
+
+const TurndownService = require('turndown');
+
+const turndown = new TurndownService();
 
 const app = express();
 
 const port = 3000;
 
 app.listen(port, () => console.log(`Listening to port ${port}!`));
+
+app.get('/', (req, res) => {
+  console.log(turndown.turndown('<h1>Hellow!</h1>'));
+});
